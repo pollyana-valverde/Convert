a0ff22946a357874a9885a6cf2c34a0247fb4d02
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-03 18:00:39 -0300

    Entradas definidas

---
+++ script.js
@@ -0,0 +1,13 @@
+//Cotação de moedas do dia
+const USD = 5.34;
+const EUR = 6.27;
+const GBP = 7.19;
+
+//Obtendo os elementos
+const form = document.querySelector("form");
+const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
+const footer = document.querySelector("main footer");
+const description = document.querySelector("#description");
+const result = document.querySelector("#result");
+
